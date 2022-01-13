0a12f3a33d7a9056bc3e3046c5735e7a853b9cae
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2022-01-13 10:20:20 -0500

    add

--- hw9-1-1.py
+++ hw9-1-1.py
@@ -1,8 +1,8 @@
 #author: LM (AMDG) 1/9/22
 def even_number(lst):
-    newlst =[]
     for index, value in enumerate(lst):
         if index % 2 == 0:
-            value = newlst.append
+            return(value)
+            
             
 print(even_number(['me', 'you', 6, 8, 3, 33, 1, 9, 7, 5, 2]))
