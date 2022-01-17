0ca4225ea5ee885d82cb3f22488788af6f63eddc
p22mendizabal <p22lmendizabal@fairfieldprep.org>
2022-01-17 11:23:43 -0500

    add file

--- hw9-1-3.py
+++ hw9-1-3.py
@@ -1,16 +1,17 @@
-#author: LM (AMDG) 1/14/22
+# author: LM (AMDG) 1/14/22
 
-def find_thing(lst):
-    i = 0
-    user_find = str(input("Enter the substring or element you want to find: "))
-    for element in str(lst):
-        if user_find in element:
-            element = lst.index(user_find)
-            print("The index of {0} is:{1}".format(user_find, element))
-        else:
-            print(-1)
-            break
-        
+def find_thing(item, search):
+    for index, value in enumerate(item):
+        if value == search[0]:
+            mark = index
+            check = ''
+            for x in range(len(search)):
+                check += item[mark]
+                mark += 1
+                if check == search:
+                    return index
+    return -1
 
 
-find_thing([7, 8, 9, 10])
+
+print(find_thing("apple", "ple"))
